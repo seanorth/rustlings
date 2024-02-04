1c1d584520d96e9747fa449331239f6e81bdb77e
seanorth sss <1058806659@qq.com>
2024-02-04 06:51:19 +0000

    0204

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -5,19 +5,63 @@
 
 // I AM NOT DONE
 
+// fn trim_me(input: &str) -> String {
+//     // TODO: Remove whitespace from both ends of a string!
+
+//     let mut beginIdx=0;
+//     let mut lastIdx=0;
+//     let mut triggerOnce = false;
+//     for (index,c) in input.char_indices(){
+//         if c!=' '{
+//             if triggerOnce==false{
+//             beginIdx=index;
+//             triggerOnce=true;
+//             }
+//             lastIdx=index;
+//         }
+//     }
+//     input[beginIdx..lastIdx+1].to_string()
+// }
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut begin_idx = 0;
+    let mut last_idx = input.len();
+
+    for (index, c) in input.char_indices() {
+        if !c.is_whitespace() {
+            last_idx = index;
+            break;
+        }
+    }
+
+    for (index, c) in input.char_indices().rev() {
+        if !c.is_whitespace() {
+            begin_idx = index;
+            break;
+        }
+    }
+
+    input[begin_idx..=last_idx].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string()+" world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    
+    for (index,c) in input.char_indices(){
+        if c!=' '{
+            if triggerOnce==false{
+            beginIdx=index;
+            triggerOnce=true;
+            }
+            lastIdx=index;
+        }
+    }
+    vec.into_iter().collect()
+
 }
 
 #[cfg(test)]
